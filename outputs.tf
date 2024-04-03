f6eff1ff09152eca5b2c3f1b74ea4c1abae0f6e9
beknazar007 <saitovbeknazar6@gmail.com>
2024-04-03 13:31:03 +0600

    added

--- outputs.tf
+++ outputs.tf
@@ -3,10 +3,11 @@
 #     value = "${module.bastion.ssh} -N -q -f"
 # }
 
-# output "configure_kubectl_command_for_cluster" {
-#     value = "gcloud container clusters get-credentials ${module.google_kubernetes_cluster.name} --zone ${module.google_kubernetes_cluster.zone} --project ${module.google_kubernetes_cluster.project_id}"
-# }
+output "configure_kubectl_command_for_cluster" {
+    value = "gcloud container clusters get-credentials ${module.google_kubernetes_cluster.name} --zone ${module.google_kubernetes_cluster.zone} --project ${module.google_kubernetes_cluster.project_id}"
+}
 
+#asdfasdf
 # output "kubectl_alias_command" {
 #     description = "Command that creates an alias for kubectl using Bastion as proxy. Bastion ssh tunnel must be running."
 #     value = "alias kubectl='${module.bastion.kubectl_command}'"

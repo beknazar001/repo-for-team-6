3a92650b732ac36fc2366ed50f63f1e060952be0
beknazar007 <saitovbeknazar6@gmail.com>
2024-04-03 06:41:38 +0600

    Changes made at среда,  3 апреля 2024 г. 06:41:38 (+06)

--- create-commit.sh
+++ create-commit.sh
@@ -10,18 +10,13 @@ do
     
     echo "This is file $i" >> "file-$new_branch.txt"
 done
-
 # Stage all changes
 git add .
-
 # Commit the changes with a timestamp
 commit_message="Changes made at $(date)"
 git commit -m "$commit_message"
-
 # Create a new branch
-git checkout -b "$new_branch"
-
+git checkout -b "$new_branch" main
 # Push the changes to the new branch
 git push origin "$new_branch"
-
 echo "Changes committed and pushed to branch '$new_branch'"

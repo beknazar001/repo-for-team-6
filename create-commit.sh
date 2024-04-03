4364b7b3c2cb566900240e7f5bc18b11028447fd
beknazar007 <saitovbeknazar6@gmail.com>
2024-04-03 08:44:54 +0600

    changed commit script

--- create-commit.sh
+++ create-commit.sh
@@ -5,18 +5,20 @@ new_branch=$1
 
 # Create 10 files and make some changes to them
 touch "file-$new_branch.txt"
+git checkout -b "$new_branch" main
 for i in {1..5}
 do
     
     echo "This is file $i" >> "file-$new_branch.txt"
+    # Stage all changes
+    git add .
+    # Commit the changes with a timestamp
+    commit_message="Changes made at $(date)"
+    git commit -m "$commit_message"
+    # Create a new branch
 done
-# Stage all changes
-git add .
-# Commit the changes with a timestamp
-commit_message="Changes made at $(date)"
-git commit -m "$commit_message"
-# Create a new branch
-git checkout -b "$new_branch" main
+
+
 # Push the changes to the new branch
 git push origin "$new_branch"
 echo "Changes committed and pushed to branch '$new_branch'"
